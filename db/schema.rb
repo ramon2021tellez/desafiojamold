59e75c4a341f87c595bf6bbf2b716092f8967809
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-24 14:29:49 -0500

    feat: Comment model, views and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_192712) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_192850) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_192712) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
